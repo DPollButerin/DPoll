65a54dc878140da9853edfa644aaa69dbfd20130
ibourn <igor.bournazel@orange.fr>
2023-07-18 23:15:24 +0200

    cleaning client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,11 @@
-import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
+// import { EthProvider } from "./contexts/EthContext";
+// import Intro from "./components/Intro/";
+// import Setup from "./components/Setup";
+// import Demo from "./components/Demo";
+// import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
-      </div>
-    </EthProvider>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
